db6446a8e4c76b6acea568b2540f1ee1b5ad0491
mithunsen04 <mithunsen559@gmail.com>
2023-12-12 20:59:18 +0530

    responsive nevbar and hambarger menu done with context

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,50 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+// import './globals.css'
+// import { Inter } from 'next/font/google'
+// import Navbar from "@/components/navbar/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
+
+// export const metadata = {
+//   title: 'Blog App',
+//   description: 'The best blog app!',
+// }
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   )
+// }
+
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/providers/ThemeProvider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Lama Dev Blog App",
+  description: "The best blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeContextProvider>
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <Navbar />
+                {children}
+              </div>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProvider>
+      </body>
     </html>
-  )
+  );
 }
