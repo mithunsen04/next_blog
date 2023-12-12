1199306bec9383d8109e44f194957cdeefa557d5
mithunsen04 <mithunsen559@gmail.com>
2023-12-12 21:23:40 +0530

    featured section is done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,22 @@
-import styles from "./homepage.module.css";
+// import styles from "./homepage.module.css";
 
-export default function Home() {
-  return <div>Hello</div>;
+// export default function Home() {
+//   return <div>Hello</div>;
+// }
+
+import Link from "next/link";
+import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/Menu/Menu";
+
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
+
+  return (
+    <div className={styles.container}>
+      <Featured />
+    </div>
+  );
 }
